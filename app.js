8902b5fe442d131fa20c1434b43235890000216b
Ares Chen <arechen@microsoft.com>
2018-05-28 06:47:45 +0800

    form

--- app.js
+++ app.js
@@ -36,24 +36,47 @@ var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azu
 var bot = new builder.UniversalBot(connector);
 bot.set('storage', tableStorage);
 
-bot.dialog('/', function (session) {
-    var msg = new builder.Message(session);
-    msg.attachmentLayout(builder.AttachmentLayout.carousel);
-    var attachments = [
-        new builder.HeroCard(session)
-            .title("歡迎參加亞太技術年會")
-            .subtitle("微軟歡迎大家，希望和大家多多交流")
-            .text("這是一個卡片正文")
-            .images([builder.CardImage.create(session, "http://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2016/04/20/103564443-GettyImages-594827903.1910x1000.jpg")])
-            .buttons([
-                builder.CardAction.imBack(session, "確定", "確定"),
-                builder.CardAction.call(session, "+886xxxxxxxxx", "打電話"),
-                builder.CardAction.openUrl(session, "https://www.microsoft.com/taiwan/events/2018devdays", "打開首頁")
-                
-            ])
-    ];
-    msg.attachments(attachments);
-    session.send(msg);
-});
+// bot.dialog('/', function (session) {
+//     //1. basic
+//     //session.send('You said ' + session.message.text);
+
+//     //2. card
+//     // var msg = new builder.Message(session);
+//     // msg.attachmentLayout(builder.AttachmentLayout.carousel);
+//     // var attachments = [
+//     //     new builder.HeroCard(session)
+//     //         .title("歡迎參加亞太技術年會")
+//     //         .subtitle("微軟歡迎大家，希望和大家多多交流")
+//     //         .text("這是一個卡片正文")
+//     //         .images([builder.CardImage.create(session, "http://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2016/04/20/103564443-GettyImages-594827903.1910x1000.jpg")])
+//     //         .buttons([
+//     //             builder.CardAction.imBack(session, "確定", "確定"),
+//     //             builder.CardAction.call(session, "+886xxxxxxxxx", "打電話"),
+//     //             builder.CardAction.openUrl(session, "https://www.microsoft.com/taiwan/events/2018devdays", "打開首頁")
+//     //         ])
+//     // ];
+//     // msg.attachments(attachments);
+//     // session.send(msg);
+
+// });
 
 
+bot.dialog('/',[
+    function (session) {
+        builder.Prompts.text(session, "Hello... What's your name?");
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?");
+    },
+    function (session, results) {
+        session.userData.coding = results.response;
+        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+    },
+    function (session, results) {
+        session.userData.language = results.response.entity;
+        session.send("Got it... " + session.userData.name +
+            " you've been programming for " + session.userData.coding +
+            " years and use " + session.userData.language + ".");
+    }
+]);
